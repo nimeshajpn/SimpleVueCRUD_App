fa7645f2dfa46a8683a99f254e9c39846e52c3a8
Nadeera Nimesha <Nadeeranimesha456@gmail.com>
2022-10-31 09:19:04 +0530

    .

--- src/views/EditContent.vue
+++ src/views/EditContent.vue
@@ -6,13 +6,8 @@
 </div>
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
 
-export default defineComponent({
-    setup() {
-        
-    },
-})
+
 </script>
 
 <style>
